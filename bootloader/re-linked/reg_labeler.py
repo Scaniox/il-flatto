81ac9430f17acbcd133bda32dc917d1a973e0c3e
Scaniox <alexl.mills3@gmail.com>
2024-01-28 22:44:56 +0000

    Revert labeling part of "some relabeling"
    This reverts labeling part of  commit bbd5bf2ad99073f805421d0d658776fe1fbc92f9.

--- bootloader/re-linked/reg_labeler.py
+++ bootloader/re-linked/reg_labeler.py
@@ -1,1 +1,18 @@
-f = open("","rw")
+import re
+
+file_name = "original_decompiled_labels"
+
+
+
+f = open(file_name,"r")
+text = f.read()
+f.close()
+
+match_pattern = r"(	(in|out)	.*)0x3e"
+replace_pattern = r"\g<1>SREG"
+
+re.sub(match_pattern, replace_pattern, text)
+
+f = open(file_name,"w")
+f.write(text)
+f.close()
